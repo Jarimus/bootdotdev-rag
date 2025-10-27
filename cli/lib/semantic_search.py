21995db8f2b2896f2cf702a0cf24250ada54ee56
Jari Ahonen <jari.n.ahonen@gmail.com>
2025-10-27 20:06:33 +0200

    text embedding with the semantic search

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -3,11 +3,23 @@ from sentence_transformers import SentenceTransformer
 class SemanticSearch:
 
   def __init__(self) -> None:
-    # Load the model
     self.model = SentenceTransformer('all-MiniLM-L6-v2')
+
+  def generate_embedding(self, text: str):
+    if text.strip() == "":
+      raise ValueError("SemanticSearch - generate embedding: text input is empty")
+    embeddings = self.model.encode([text])
+    return embeddings[0]
     
 
 def verify_model():
   semantic_search = SemanticSearch()
   print(f"Model loaded: {semantic_search.model}")
   print(f"Max sequence length: {semantic_search.model.max_seq_length}")
+
+def embed_text(text: str):
+  semantic_search = SemanticSearch()
+  embedding = semantic_search.generate_embedding(text)
+  print(f"Text: {text}")
+  print(f"First 3 dimensions: {embedding[:3]}")
+  print(f"Dimensions: {embedding.shape[0]}")
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,25 +1,32 @@
 #!/usr/bin/env python3
 
-from lib.semantic_search import verify_model
+from lib.semantic_search import verify_model, embed_text
 
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser(description="Semantic Search CLI")
-    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+  parser = argparse.ArgumentParser(description="Semantic Search CLI")
+  subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
-    # Verify model command
-    subparsers.add_parser("verify", help="Verify the semantic search model")
+  # Verify model command
+  subparsers.add_parser("verify", help="Verify the semantic search model")
 
-    # Parse arguments
-    args = parser.parse_args()
+  # embed_text command
+  embed_text_subparser = subparsers.add_parser("embed_text", help="Embed text with the semantic model")
+  embed_text_subparser.add_argument("text", help="text to embed")
 
-    match args.command:
-        case "verify":
-            verify_model()
+  # Parse arguments
+  args = parser.parse_args()
 
-        case _:
-            parser.print_help()
+  match args.command:
+    case "verify":
+      verify_model()
+
+    case "embed_text":
+      embed_text(args.text)
+
+    case _:
+      parser.print_help()
 
 if __name__ == "__main__":
-    main()
+  main()
